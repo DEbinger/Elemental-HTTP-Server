8ad02d79b89e4c3505a3f96aca33edfba15a9302
David <davidebinger888@gmail.com>
2016-12-31 19:01:17 -1000

    initial commit of files

---
+++ server.js
@@ -0,0 +1,56 @@
+/*jshint esversion:6*/
+
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+
+const fileNotFoundErrorHandler = (res) => {
+  res.statusCode = 500;
+  res.end('Server is broken');
+};
+
+const sendContent = (res, content) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write(content);
+  res.end();
+};
+
+const resourceMapping = {
+  '/styles' : './public/css/styles.css',
+  '/helium' : './public/helium.html',
+  '/hydrogen' : './public/hydrogen.html',
+  '/' : './public/index.html',
+  '/404' : './public/404.html',
+
+};
+
+
+const server = http.createServer( (req, res) => {
+  console.log("req.url", req.url);
+  console.log("req.method", req.method);
+  console.log("req.headers", req.headers);
+
+  let reqBody = '';
+  req.setEncoding('utf8');
+  req.on('data', (chunk) => {
+    reqBody += chunk;
+  });
+
+if (resourceMapping.hasOwnProperty(req.url)) {
+    fs.readFile(resourceMapping[req.url] || '', (err, content) => {
+      if(err){
+        res.statusCode = 404;
+        res.write(res, 'Resource not found');
+        return;
+      }
+
+      sendContent(res, content);
+    });
+  }
+  });
+
+
+
+server.listen(PORT, () => {
+  console.log("server is listening on port", PORT);
+});
