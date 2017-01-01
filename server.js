fd6558446cf5b2407b3f6cbc63546b8e65dfb3db
David <davidebinger888@gmail.com>
2016-12-31 20:04:42 -1000

    still grinding to get rid of resourceMapping

--- server.js
+++ server.js
@@ -15,15 +15,13 @@ const sendContent = (res, content) => {
   res.end();
 };
 
-const resourceMapping = {
+/*const resourceMapping = {
   '/styles' : './public/css/styles.css',
   '/helium' : './public/helium.html',
   '/hydrogen' : './public/hydrogen.html',
   '/' : './public/index.html',
-  '/404' : './public/404.html',
-
-};
-
+  '/404' : './public/404.html'
+};*/
 
 const server = http.createServer( (req, res) => {
   console.log("req.url", req.url);
@@ -34,21 +32,54 @@ const server = http.createServer( (req, res) => {
   req.setEncoding('utf8');
   req.on('data', (chunk) => {
     reqBody += chunk;
+    console.log('chunk');
+  });
+  req.on('end', () => {
+    // reqBody is complete, handle request!
+    console.log(reqBody);
+
+  if( req.url === '/' ){
+      fs.readFile('./index.html', (err, content) => {
+        if(err){
+          res.statusCode = 500;
+          res.end('Server is broken');
+          return;
+        }
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write(content);
+        res.end();
+      });
+    } else if( req.url === '/helium' ){
+      fs.readFile('./helium.html', (err, content) => {
+        if(err){
+          res.statusCode = 500;
+          res.end('Server is broken');
+          return;
+        }
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write(content);
+        res.end();
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('Resource not found');
+      return;
+    }
   });
 
-if (resourceMapping.hasOwnProperty(req.url)) {
-    fs.readFile(resourceMapping[req.url] || '', (err, content) => {
-      if(err){
+/*
+    fs.readFile(resourceMapping[req.url] || '', (err, content) => {*/
+ /*     if(err){
         res.statusCode = 404;
-        res.write(res, 'Resource not found');
+        sendContent(res, 'Resource not found');
         return;
-      }
+      }*/
 
-      sendContent(res, content);
-    });
-  }
-  });
-
+//      sendContent(res, content);
+});
 
 
 server.listen(PORT, () => {
